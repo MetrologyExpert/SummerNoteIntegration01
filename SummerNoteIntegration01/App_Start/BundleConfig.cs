2bdafe72123ce364d1450b190f0162fc67ec933c
Peter Benko <Peter Benko@DESKTOP-6L6GM63>
2019-12-06 15:37:03 +0000

    Table change to post cards and added new CSS style from bootwatch.com (Cerulean)

--- SummerNoteIntegration01/App_Start/BundleConfig.cs
+++ SummerNoteIntegration01/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SummerNoteIntegration01
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-c.css",
                       "~/Content/site.css"));
         }
     }
